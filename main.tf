ddf09bda365b75bfa6cfe8a6fd87635fa4606610
Pushkar210 <116288761+Pushkar210@users.noreply.github.com>
2022-12-02 18:36:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,9 +77,9 @@ resource "aws_security_group" "Group02-DevSecOps1" {
 
 resource "aws_instance" "app_server" {
   ami                    = "ami-096800910c1b781ba"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.large"
   key_name               = "x21177279"
-  vpc_security_group_ids = ["${aws_security_group.Group11-DevSecOps1.id}"]
+  vpc_security_group_ids = ["${aws_security_group.Group02-DevSecOps1.id}"]
   # user_data              = file("temp.sh")
   connection {
     type        = "ssh"
